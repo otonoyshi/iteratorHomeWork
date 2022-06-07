1a79e73d7b6a0b95f26a544678cd4e488f835b6f
Georgy <georgy.matskevich@gmail.com>
2022-06-07 19:02:44 +0300

    second commit

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,47 +1,30 @@
-import java.util.*;
-import java.util.function.Consumer;
+import java.util.Iterator;
+import java.util.Random;
 
-public class Randoms implements Iterable<Integer>{
+public class Randoms implements Iterable<Integer> {
     protected Random random;
     protected int max;
     protected int min;
-    private List<Integer> list = new ArrayList<>();
-    public Randoms(int min, int max){
 
+    public Randoms(int min, int max) {
         this.max = max;
         this.min = min;
-
     }
 
     @Override
     public Iterator<Integer> iterator() {
         random = new Random();
-
-        return new Iterator<>(){
-            boolean isList = true;
-            int next = 0;
+        return new Iterator<>() {
 
 
             @Override
             public boolean hasNext() {
-                if (isList){
-                    list.add(random.nextInt(min,max + 1));
-                    if (next < list.size())
-                        return true;
-                }
-                return false;
+                return true;
             }
 
             @Override
             public Integer next() {
-                if (isList){
-                   if (next < list.size()){
-                       int result = list.get(next);
-                       next++;
-                       return result;
-                    }
-                }
-                return 0;
+                return random.nextInt(min, max + 1);
             }
         };
     }
